a064e812f310d787e1ceeeca582a5a5b41ddc056
Maksim Sosnovskiy <maxim.sosnovsky@gmail.com>
2020-10-20 01:07:57 +0300

    added burger menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,4 @@
 document.querySelector('.header__content').onclick = function () {
+  document.querySelector('.header__menu-icon').classList.toggle('header__menu-icon_active');
   document.querySelector('.header__navigation').classList.toggle('header__navigation_active');
 }
