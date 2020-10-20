e163c215752afe56e79289049621542f3520b7c3
Maksim Sosnovskiy <SosnovskiyMV@quarta.su>
2020-10-20 13:18:36 +0300

    added animation for menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,20 @@
-document.querySelector('.header__content').onclick = function () {
+const menu = document.querySelector('.header__content');
+const menuBurgerButton = document.querySelector('.header__menu-icon');
+const windowsWidth = document.documentElement.clientWidth;
+
+menuBurgerButton.onclick = function () {
   document.querySelector('.header__menu-icon').classList.toggle('header__menu-icon_active');
   document.querySelector('.header__navigation').classList.toggle('header__navigation_active');
+  document.querySelector('.header').classList.toggle('background_black');;
+}
+
+let scrolled;
+window.onscroll = function () {
+  scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrolled > 100 && windowsWidth <= 600 ) {
+    menu.style.backgroundColor = '#1A1B22';
+  }
+  if (100 > scrolled && windowsWidth <= 600 ) {
+    menu.style.backgroundColor = 'transparent';
+  }
 }
