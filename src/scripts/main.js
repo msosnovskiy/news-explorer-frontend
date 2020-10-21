177b7a3e9e9c688ca297bd4458e0701467251b5c
Maksim Sosnovskiy <maxim.sosnovsky@gmail.com>
2020-10-21 22:54:03 +0300

    edit header menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,40 +5,54 @@ const logo = document.querySelector('.logo');
 const menuBurgerButton = document.querySelector('.header__menu-icon');
 const windowsWidth = document.documentElement.clientWidth;
 
+// function menuOpened() {
+//   console.log(navigation.classList.contains(('header__navigation_active')));
+//   return navigation.classList.contains(('header__navigation_active'));
+// }
+
 function menuOpened() {
   return navigation.classList.contains(('header__navigation_active'));
 }
 
+
 // затемнение меню при прокрутке на мобильной версии
-let scrolled = 0;
+
+function scrolled() {
+  let scrolledСounter = 0;
+  scrolledСounter = window.pageYOffset || document.documentElement.scrollTop;
+  return scrolledСounter;
+}
+
 window.onscroll = function () {
-  scrolled = window.pageYOffset || document.documentElement.scrollTop;
-  if (scrolled >= 60 && windowsWidth <= 600 && !menuOpened()) {
-    if (header.classList.contains('header_black')) {
-      menuBurgerButton.classList.remove('header__menu-icon_black');
-      menuBurgerButton.classList.add('header__menu-icon_white');
+  if (!menuOpened()) {
+    if (scrolled() >= 60 && windowsWidth <= 600) {
+      if (header.classList.contains('header_black')) {
+        menuBurgerButton.classList.remove('header__menu-icon_black');
+        menuBurgerButton.classList.add('header__menu-icon_white');
+        menu.style.backgroundColor = '#1A1B22';
+        logo.classList.remove('logo_black');
+        logo.classList.add('logo_white');
+        return;
+      }
       menu.style.backgroundColor = '#1A1B22';
-      logo.classList.remove('logo_black');
-      logo.classList.add('logo_white');
-      return;
     }
-    menu.style.backgroundColor = '#1A1B22';
-  }
-  if (60 > scrolled && windowsWidth <= 600 && !menuOpened()) {
-    if (header.classList.contains('header_black')) {
-      menuBurgerButton.classList.remove('header__menu-icon_white');
-      menuBurgerButton.classList.add('header__menu-icon_black');
+    if (60 > scrolled() && windowsWidth <= 600) {
+      if (header.classList.contains('header_black')) {
+        menuBurgerButton.classList.remove('header__menu-icon_white');
+        menuBurgerButton.classList.add('header__menu-icon_black');
+        menu.style.backgroundColor = 'transparent';
+        logo.classList.add('logo_black');
+        logo.classList.remove('logo_white');
+        return;
+      }
       menu.style.backgroundColor = 'transparent';
-      logo.classList.add('logo_black');
-      logo.classList.remove('logo_white');
-      return;
     }
-    menu.style.backgroundColor = 'transparent';
   }
 }
 
 //открытие меню и смена логотипа
 const openMenu = () => {
+
   menuBurgerButton.classList.toggle('header__menu-icon_active');
   navigation.classList.toggle('header__navigation_active');
   header.classList.toggle('background_black');
@@ -49,15 +63,25 @@ const openMenu = () => {
     if (logo.classList.contains('logo_black')) {
       logo.classList.remove('logo_black');
       logo.classList.add('logo_white');
+      return;
     }
     // смена цвета логотипа, если меню в начале открыли и сразу закрыли
-    if (scrolled <= 60 && !menuOpened()) {
+    if (scrolled() < 60 && !menuOpened()) {
       logo.classList.remove('logo_white');
       logo.classList.add('logo_black');
-      console.log(scroled)
+      menu.style.backgroundColor = 'transparent';
+      menuBurgerButton.classList.add('header__menu-icon_black');
+      return;
     }
-
-    else return;
+    // смена цвета логотипа, если меню закрыли после скрола
+    if (scrolled() >= 60 && !menuOpened()) {
+      menu.style.backgroundColor = '#1A1B22';
+      menuBurgerButton.classList.remove('header__menu-icon_black');
+      menuBurgerButton.classList.add('header__menu-icon_white');
+    }
+    else {
+      return;
+    }
   }
 
 }
